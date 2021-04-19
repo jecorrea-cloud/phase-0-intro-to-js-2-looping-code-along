7facc2c9f6bff4a30f82a5b17a3c8a6390925ebe
joaquin <jecorrea@protonmail.com>
2021-04-19 01:53:36 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let thanksMessages = [];
+    for (let i = 0; i < names.length; i++) {
+      thanksMessages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+      
+    }
+    return thanksMessages;
+  }
+
+  function countDown(number){
+    let counter = number;
+    while(counter >= 0){
+      console.log(counter);
+      counter -= 1;
+    }
+  }
